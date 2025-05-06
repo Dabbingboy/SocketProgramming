ac6c6087a8f3a6fdee67e6d31a2622c9dfd00ddf
Manu Durga <75253222+Dabbingboy@users.noreply.github.com>
2025-05-06 07:43:15 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+HEADER = 64
+PORT = 5050
+SERVER = "192.168.0.101" #SERVER IP
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER,PORT)#address format 
+
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#Socket contruction
+try:
+    client.connect(ADDR)
+#Error Handling if Server is not available but client is starting,
+#sys used to stop the program
+except:
+    print("SERVER UNAVAILABLE")
+    sys.exit()
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length =len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length = send_length + b' ' * (HEADER - len(send_length))#encoding space character i.e b' ', to fill out empty spaces in message, hello as'
+    client.send(send_length)#                                      hello as b'hello                                                           '
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+print("MESSAGE:",end=' ')
+send(input(""))
+input()#for user to press enter and exit the program
+send(DISCONNECT_MESSAGE)
---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())#Automatically gets IP for server
+ADDR = (SERVER,PORT)#address format 
+FORMAT = 'utf-8'
+#utf-8 is used to encode because it formats the string into its ascii value,
+#further converted in binary form.
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#socket construction
+server.bind(ADDR)
+def handle_client(conn, addr):
+    print(f"\nNEW CONNECTION {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"{addr} {msg.rstrip()}")
+            conn.send("MESSAGE RECEIVED".encode(FORMAT))
+    conn.close()
+    print(f"DISCONNECTING {addr[0]}....")
+    
+def start():
+    server.listen()
+    print(f"LISTENING, the server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print(f"\nACTIVE CONNECTION ,{threading.active_count()-2}")
+        
+#each time handle_client as a thread is started it is because a new client has connected
+#Threading is used to branch out the flow of the code from the main code to just a function,
+#optimal in handling multiple clients for a single server 
+print("STARTING, the server is starting...")
+start() 

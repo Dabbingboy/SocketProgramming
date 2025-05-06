e84bc18b4be8e1ebdf1f949b35693a24c369f076
Manu Durga <75253222+ManuDurga@users.noreply.github.com>
2025-05-06 14:14:57 +0530

    client.py

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ import socket
 import sys
 HEADER = 64
 PORT = 5050
-SERVER = "X.X.X.X" #SERVER IPv4
+SERVER = "192.168.0.101" #SERVER IPv4
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 ADDR = (SERVER,PORT)#address format 
-
+t_bool=False
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#Socket contruction
 try:
@@ -16,6 +16,15 @@ try:
 except:
     print("SERVER UNAVAILABLE")
     sys.exit()
+
+def receive(client):
+    message = client.recv(HEADER).decode(FORMAT)
+    msg_length =len(message)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg=client.recv(msg_length).decode(FORMAT)
+        print(msg)
+
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length =len(message)
@@ -23,26 +32,26 @@ def send(msg):
     send_length = send_length + b' ' * (HEADER - len(send_length))#encoding space character i.e b' ', to fill out empty spaces in message, hello as'
     client.send(send_length)#                                      hello as b'hello                                                           '
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
-def talk(t_bool):
+
+def talk():
+    print(client.recv(HEADER).decode(FORMAT))
+    global t_bool
     print("To exit talk mode, type 'talk over'")
+    f_time=True
     while t_bool:
         msg=input("MESSAGE: ")
-        message = msg.encode(FORMAT)
-        msg_length =len(message)
-        send_length = str(msg_length).encode(FORMAT)
-        send_length = send_length + b' ' * (HEADER - len(send_length))#encoding space character i.e b' ', to fill out empty spaces in message, hello as'
-        client.send(send_length)#                                      hello as b'hello                                                           '
-        client.send(message)
-        if client.recv(64).decode(FORMAT)=="T4LK_0V3R":
+        if msg.lower()=="talk over":
             t_bool=False
-        else:
-        print(client.recv(64).decode(FORMAT))
-    
+            send(msg)
+            return
+        send(msg)
+        receive(client)
+        
 ip=input("MESSAGE:")
 if ip.lower() == 'talk':
-    ip == "!TALK"
+    ip = "!TALK"
+    send(ip)
+    t_bool=True
     talk()
-send(ip)
 input()#for user to press enter and exit the program
 send(DISCONNECT_MESSAGE)
